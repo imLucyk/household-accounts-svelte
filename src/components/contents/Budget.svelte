b054bba42a0eb9f1d1804c7009e5419a05028896
develucy <develucy@gmail.com>
2022-06-27 21:40:19 +0900

    [#4] 라우터 연결

---
+++ src/components/contents/Budget.svelte
@@ -0,0 +1,14 @@
+<section class="budget">
+  <article class="guide">
+    <div class="title">한 달 예산을 세워볼까요?</div>
+    <div class="subtitle">예산을 설정하고 계획적으로 관리 해보세요.</div>
+  </article>
+  <article class="monthly-budget">
+    <span class="monthly-budget">한 달 예산</span>
+    <span class="total-monthly-budget">0 원</span>
+  </article>
+  <article class="bottons">
+    <button class="copy-budget">이전 예산 복사하기</button>
+    <div class="pre-month-budget">4월 예산 보러가기</div>
+  </article>
+</section>
---
+++ src/components/contents/Details.svelte
@@ -0,0 +1,58 @@
+<section class="payments">
+  <div class="filter">
+    <span class="full-details">전체 내역<span class="material-icons font-16">expand_more</span></span>
+    <span class="add"><span class="material-icons font-16">add</span>내역 추가</span>
+  </div>
+  <article class="daily">
+    <div class="daily-title">
+      <span class="day">16일 오늘</span>
+      <span class="payment">-5,400원</span>
+    </div>
+    <div class="daily-detail">
+      <div><img src="./images/coffee.png" width="35" alt="Coffee"></div>
+      <div class="daily-detail-goods">
+        <div class="place">크리스피크림 남영역</div>
+        <div class="note">도넛/핫도그 | 프렌즈 체크카드</div>
+      </div>
+      <div class="daily-payment">-5,400원</div>
+    </div>
+  </article>
+  <article class="daily">
+    <div class="daily-title">
+      <span class="day">15일 일요일</span>
+      <span class="payment">-57,150원</span>
+    </div>
+    <div class="daily-detail">
+      <div><img src="./images/food.png" width="35" alt="Food"></div>
+      <div class="daily-detail-goods">
+        <div class="place">네번째집</div>
+        <div class="note">한식 | 프렌즈 체크카드</div>
+      </div>
+      <div class="daily-payment">-35,000원</div>
+    </div>
+    <div class="daily-detail">
+      <div><img src="./images/basket.png" width="35" alt="Cart"></div>
+      <div class="daily-detail-goods">
+        <div class="place">씨유이태원점</div>
+        <div class="note">편의점 | 프렌즈 체크카드</div>
+      </div>
+      <div class="daily-payment">-3,900원</div>
+    </div>
+    <div class="daily-detail">
+      <div><img src="./images/basket.png" width="35" alt="Cart"></div>
+      <div class="daily-detail-goods">
+        <div class="place">씨유이태원점</div>
+        <div class="note">편의점 | 프렌즈 체크카드</div>
+      </div>
+      <div class="daily-payment">-4,500원</div>
+    </div>
+    <div class="daily-detail">
+      <div><img src="./images/basket.png" width="35" alt="Cart"></div>
+      <div class="daily-detail-goods">
+        <div class="place">씨유이태원점</div>
+        <div class="note">편의점 | 프렌즈 체크카드</div>
+      </div>
+      <div class="daily-payment">-13,750원</div>
+    </div>
+  </article>
+</section>
