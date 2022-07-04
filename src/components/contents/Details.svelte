cbf0088764ba508e82fc531a0660240dac29cafb
develucy <develucy@gmail.com>
2022-07-04 20:08:30 +0900

    [#2] 모달창 마크업 추가

--- src/components/contents/Details.svelte
+++ src/components/contents/Details.svelte
@@ -2,12 +2,17 @@
 import detailsStore from '../../stores/detailsStore';
 const {details, detail} = detailsStore;
 console.log($details, $detail);
+
+const modalToggle = function() {
+  document.body.classList.toggle('o-hidden');
+  document.getElementsByClassName('modal-background')[0].classList.toggle('active');
+}
 </script>
 
 <section class="payments">
   <div class="filter">
     <span class="full-details">전체 내역<span class="material-icons font-16">expand_more</span></span>
-    <span class="add"><span class="material-icons font-16">add</span>내역 추가</span>
+    <span class="add" on:click="{() => modalToggle()}"><span class="material-icons font-16">add</span>내역 추가</span>
   </div>
   <article class="daily">
     <div class="daily-title">
@@ -62,3 +67,35 @@ console.log($details, $detail);
     </div>
   </article>
 </section>
+
+<div class="modal-background" on:click="{() => modalToggle()}">
+  <form class="modal" on:click="{(event) => event.stopPropagation()}">
+    <h3 class="modal-header">Edit</h3>
+    <table class="modal-table">
+      <tbody>
+        <tr>
+          <th>
+            <span>Name</span>
+          </th>
+          <td><input type="text" name="grocery-name" placeholder="Name" /></td>
+        </tr>
+        <tr>
+          <th>
+            <span>Enter</span>
+          </th>
+          <td><input type="date" name="grocery-enter" placeholder="YYYY-MM-DD" /></td>
+        </tr>
+        <tr>
+          <th>
+            <span>Expire</span>
+          </th>
+          <td><input type="date" name="grocery-expire" placeholder="YYYY-MM-DD" /></td>
+        </tr>
+      </tbody>
+    </table>
+    <div class="modal-footer">
+      <button class="button-close" type="button" on:click="{() => modalToggle()}"><span class="material-icons">close</span></button>
+      <button class="button-update" type="button"><span class="material-icons">edit_note</span></button>
+    </div>
+  </form>
+</div>
