0e23d3c6d610097b3a9d93a3062041ed214e3e95
develucy <develucy@gmail.com>
2022-07-11 21:36:53 +0900

    [#10] 캘린더 월 변경

--- src/components/contents/Calendar.svelte
+++ src/components/contents/Calendar.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 import moment from 'moment';
-const calcDay = function(line, day) {
-  const firstDay = moment().startOf('month');
-  const calc = line*7 + day - firstDay.day();
-  return moment(firstDay).add(calc, 'days').format('D');
+import calendarStore from '../../stores/calendarStore';
+
+const {firstDay} = calendarStore;
+
+$: calcDay = function(line, day) {
+  // const firstDay = moment().startOf('month');
+  const calc = line*7 + day - $firstDay.day();
+  return moment($firstDay).add(calc, 'days').format('D');
 }
 </script>
 
@@ -26,92 +30,6 @@ const calcDay = function(line, day) {
       <div class="sum">쓴돈</div>
     </div>
     {/each}
-    <!-- <div class="days">
-      <div class="number">2</div>
-      <div class="sum"></div>
-    </div>
-    <div class="days">
-      <div class="number">3</div>
-      <div class="sum">-128,500원</div>
-    </div>
-    <div class="days">
-      <div class="number">4</div>
-      <div class="sum">-4,500원</div>
-    </div>
-    <div class="days">
-      <div class="number">5</div>
-      <div class="sum">-125,600원</div>
-    </div>
-    <div class="days">
-      <div class="number">6</div>
-      <div class="sum">-128,500원</div>
-    </div>
-    <div class="days">
-      <div class="number">7</div>
-      <div class="sum">-128,500원</div>
-    </div> -->
   </article>
   {/each}
-  <!-- <article class="weekly-sum">-391,9300원</article>
-  <article class="week">
-    <div class="days">
-      <div class="number">8</div>
-      <div class="sum">-23,800원</div>
-    </div>
-    <div class="days">
-      <div class="number">9</div>
-      <div class="sum"></div>
-    </div>
-    <div class="days">
-      <div class="number">10</div>
-      <div class="sum">-128,500원</div>
-    </div>
-    <div class="days">
-      <div class="number">11</div>
-      <div class="sum">-4,500원</div>
-    </div>
-    <div class="days">
-      <div class="number">12</div>
-      <div class="sum">-125,600원</div>
-    </div>
-    <div class="days">
-      <div class="number">13</div>
-      <div class="sum">-128,500원</div>
-    </div>
-    <div class="days">
-      <div class="number">14</div>
-      <div class="sum">-128,500원</div>
-    </div>
-  </article>
-  <article class="weekly-sum">-62,550원</article>
-  <article class="week">
-    <div class="days">
-      <div class="number">15</div>
-      <div class="sum">-23,800원</div>
-    </div>
-    <div class="days">
-      <div class="number active">16</div>
-      <div class="sum"></div>
-    </div>
-    <div class="days">
-      <div class="number unselect">17</div>
-      <div class="sum">-128,500원</div>
-    </div>
-    <div class="days">
-      <div class="number unselect">18</div>
-      <div class="sum">-4,500원</div>
-    </div>
-    <div class="days">
-      <div class="number unselect">19</div>
-      <div class="sum">-125,600원</div>
-    </div>
-    <div class="days">
-      <div class="number unselect">20</div>
-      <div class="sum">-128,500원</div>
-    </div>
-    <div class="days">
-      <div class="number unselect">21</div>
-      <div class="sum">-128,500원</div>
-    </div>
-  </article> -->
 </section>
